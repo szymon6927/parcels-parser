1a901b8ad7389392c8ea59527258cb7915df6e39
Szymon Miks <miks.szymon@gmail.com>
2019-01-06 10:00:30 +0100

    add test for column exist and file doesnt exist

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -14,6 +14,19 @@ class TestPracelsParser(unittest.TestCase):
 
         self.assertTrue(file_path, os.path.isfile(file_path))
 
+    def test_if_file_doesnt_exist(self):
+        self.parser.set_file("./test_cadastral_parcels_wrong.tsv")
+        file_path = file_path = self.parser.get_file()
+
+        self.assertTrue(file_path, os.path.isfile(file_path))
+
+    def test_if_column_exist(self):
+        dirpath = os.path.dirname(os.path.abspath(__file__))
+        filepath = os.path.join(dirpath, self.parser.get_file())
+        df = pd.read_csv(filepath, sep='\t')
+
+        self.assertTrue(True, self.parser.get_column_name() in df.columns)
+
 
 if __name__ == '__main__':
     unittest.main()

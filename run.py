655647933557efc47a8b82dd1b573834ccdc499d
Szymon Miks <miks.szymon@gmail.com>
2019-01-06 09:31:33 +0100

    add run.py file with python argparser implementation and run main application

---
+++ run.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""
+Author: Szymon Miks
+05.01.2019
+Recruitment task for company: ADGROUP
+"""
+
+import argparse
+from application.ParcelsParser import ParcelsParser
+
+
+def arg_parser():
+    description = """Application for decoding 
+    county_code, commune_code, commune_type, district_number, parcel_number
+    based on cadastral parcel identifier.
+    Importat column names in the file are required
+    """
+
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument("--filename", help="path to the file with data", default="cadastral_parcels.tsv")
+    parser.add_argument("--column", help="name of the column in file where data are located",
+                        default="cadastral_parcel_identifier")
+
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        exit()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = arg_parser()
+    parser = ParcelsParser(args.filename, args.column)
+
+    parser.get_identifiers_data()
+    parser.extract_data()
+
+    print(parser)
+    parser.beautify_print()
